41e0b0b0228d0a33c646cef1040bcf93a9e84420
mlaskowski17 <marcin.ladesign@gmail.com>
2021-07-09 15:45:17 +0200

    fix: print loading weights steps in cortex/answer_generator

--- cortex/answer_generator/predictor.py
+++ cortex/answer_generator/predictor.py
@@ -1,22 +1,27 @@
 import wget
 import torch
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2Config
 import generator
+
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, GPT2Config
 
 
 class PythonPredictor:
     def __init__(self, config):
         medium_config = GPT2Config(n_embd=1024, n_layer=24, n_head=16)
         model = GPT2LMHeadModel(medium_config)
+
+        print("Step 1/3: Downloading weights [823 MB]...")
         wget.download(
             "https://convaisharables.blob.core.windows.net/lsp/multiref/medium_ft.pkl",
             "/tmp/medium_ft.pkl",
         )
 
+        print("Step 2/3: Loading weights...")
         weights = torch.load("/tmp/medium_ft.pkl")
         weights["lm_head.weight"] = weights["lm_head.decoder.weight"]
         weights.pop("lm_head.decoder.weight", None)
 
+        print("Step 3/3: Loading a model...")
         model.load_state_dict(weights)
 
         device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -27,6 +32,7 @@ class PythonPredictor:
         self.device = device
         self.model = model
         self.tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+        print("Model is ready!")
 
     def predict(self, payload):
         conditioned_tokens = self.tokenizer.encode(payload["text"]) + [generator.END_OF_TEXT]
